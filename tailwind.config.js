535abc9d11fe1b6caa6f15093076170a84a619dd
Asaromi <a.sabililromi@gmail.com>
2023-11-02 05:53:13 +0700

    fix: tailwind custom style and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,41 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+  mode: 'jit',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         amazon_blue: {
-          light: "#232F3E",
-          DEFAULT: "#131921",
+          light: '#232F3E',
+          DEFAULT: '#131921',
         },
       },
       height: {
-        fhd: "1080px",
+        '50': '200px',
+        fhd: '1080px',
       },
       width: {
-        "50": '200px',
-        fhd: "1920px",
+        '50': '200px',
+        fhd: '1920px',
       },
     },
-    maxHeight: (theme) => ({
-      ...theme("height"),
+    maxHeight: ({theme}) => ({
+      ...theme('height'),
     }),
-    minHeight: (theme) => ({
-      ...theme("height"),
+    minHeight: ({theme}) => ({
+      ...theme('height'),
     }),
-    maxWidth: (theme) => ({
-      ...theme("width"),
+    maxWidth: ({theme}) => ({
+      ...theme('width'),
     }),
-    minWidth: (theme) => ({
-      ...theme("width"),
+    minWidth: ({theme}) => ({
+      ...theme('width'),
     }),
   },
   variants: {
     extend: {},
   },
-};
+}
