0b506fb226e2b078fc7a7664bd1e3040e6d9f1b0
Asaromi <a.sabililromi@gmail.com>
2023-11-02 05:51:37 +0700

    fix: replace server side rendering to client side rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,41 @@
+'use client'
+
 import Head from 'next/head'
 import {useEffect, useState} from 'react'
 
 import {Banner, Header, ProductFeed} from '../components'
 import {products as exampleProducts} from '../data/products'
 
-const Home = ({products}) => {
+const Home = () => {
   const [clientRender, setClientRender] = useState(false)
+  const [products, setProducts] = useState([])
 
   const beforeDestroy = () => {
     document.body.style.overflow = null
   }
 
+  const getProducts = async () => {
+    const productsData = (await fetch('https://fakestoreapi.com/products')
+        .then(
+          async (res) => (await res.json()),
+        )
+        .catch((error) => {
+          console.log('getting error while fetching products from fakestoreapi.com')
+          console.error(error)
+
+          return null
+        })
+    )
+
+    setProducts(() => (productsData || exampleProducts))
+  }
+
   useEffect(() => {
     setClientRender(true)
+    getProducts()
 
     return beforeDestroy
-  })
+  }, [])
 
   return clientRender && (
     <div className="bg-gray-100 min-h-screen overflow-y-auto overflow-x-hidden">
@@ -34,26 +54,6 @@ const Home = ({products}) => {
       </main>
     </div>
   ) || null
-}
-
-export const getServerSideProps = async () => {
-  const products = (await fetch('https://fakestoreapi.com/products')
-    .then(
-      async (res) => (res && await res.json() || exampleProducts),
-    )
-    .catch((error) => {
-      // console.log('getting error while fetching products from fakestoreapi.com')
-      // console.error(error)
-
-      return exampleProducts
-    })
-  )
-
-  return {
-    props: {
-      products,
-    },
-  }
 }
 
 export default Home
