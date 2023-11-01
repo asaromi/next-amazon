adca46a219178b8448968349bdaa8827c3a694e9
Asaromi <a.sabililromi@gmail.com>
2023-11-02 06:14:20 +0700

    fix: new next config

--- next.config.js
+++ next.config.js
@@ -2,19 +2,23 @@
 
 const nextConfig = {
   images: {
-    domains: ['links.papareact.com', 'fakestoreapi.com'],
+    domains: ['links.papareact.com', 'fakestoreapi.com', 'res.cloudinary.com'],
     remotePatterns: [{
       hostname: 'pnghd.pics',
       protocol: 'https',
     }],
+    loader: 'custom',
   },
   env: {
-    RUNTIME_MODE: process.env.RUNTIME_MODE,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
-    GOOGLE_ID: process.env.GOOGLE_ID || 'google-id',
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'google-secret',
+    API_URL: process.env.API_URL || 'http://localhost:3000/api',
     SECRET: process.env.SECRET || undefined,
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY || 'stripe-public-key',
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || 'stripe-secret-key',
+    STRIPE_SHIPPING_RATE: process.env.STRIPE_SHIPPING_RATE || 'stripe-shipping-rate',
+    BASE_URL: process.env.HOST || 'http://localhost:3000',
   },
+  output: 'export',
+  transpilePackages: ["next-image-export-optimizer"]
 }
 
 module.exports = nextConfig
