d10022a0c9a97371ee653ab35575778c97762e09
Asaromi <a.sabililromi@gmail.com>
2023-11-05 14:07:00 +0700

    fix: update id with number

--- src/data/products.js
+++ src/data/products.js
@@ -1,8 +1,8 @@
-const { v4: uuid } = require('uuid')
+
 
 exports.products = [
   {
-    id: uuid(),
+    id: 1,
     title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
     price: 109.95,
     rating: 4,
@@ -12,7 +12,7 @@ exports.products = [
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
   },
   {
-    id: uuid(),
+    id: 2,
     title: "Mens Casual Premium Slim Fit T-Shirts ",
     price: 22.3,
     rating: 4,
@@ -22,7 +22,7 @@ exports.products = [
     image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
   },
   {
-    id: uuid(),
+    id: 3,
     title: "Mens Cotton Jacket",
     price: 55.99,
     rating: 3,
@@ -32,7 +32,7 @@ exports.products = [
     image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
   },
   {
-    id: uuid(),
+    id: 4,
     title: "Mens Casual Slim Fit",
     price: 15.99,
     rating: 1,
@@ -42,7 +42,7 @@ exports.products = [
     image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
   },
   {
-    id: uuid(),
+    id: 5,
     title: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
     price: 695,
     rating: 1,
@@ -52,7 +52,7 @@ exports.products = [
     image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
   },
   {
-    id: uuid(),
+    id: 6,
     title: "Solid Gold Petite Micropave ",
     price: 168,
     rating: 3,
@@ -62,7 +62,7 @@ exports.products = [
     image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
   },
   {
-    id: uuid(),
+    id: 7,
     title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
     price: 599,
     rating: 3,
