4fa6edb2e7d85b74829110f70dc32acf719f380b
Asaromi <a.sabililromi@gmail.com>
2023-11-05 21:06:56 +0700

    fix: product loader

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -3,9 +3,15 @@ import Product from './Product'
 import ProductLoader from './ProductLoader'
 
 const ProductFeed = ({products = []}) => {
+  const [clientRender, setClientRender] = React.useState(false)
+
+  React.useEffect(() => {
+    setClientRender(() => !products.length)
+  }, [products])
+
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 -mt-5 md:-mt-52">
-      {products.length > 0 && products.slice(0, 4).map(({id, title, price, description, category, image}) => (
+      {!clientRender && products.slice(0, 4).map(({id, title, price, description, category, image}) => (
         <Product
           key={id}
           id={id}
@@ -19,13 +25,17 @@ const ProductFeed = ({products = []}) => {
         <ProductLoader key={i} />
       ))}
 
-      <img
-        className="w-full max-w-hdPlus md:col-span-full px-5"
-        src="https://links.papareact.com/dyz"
-        alt="banner"
-      />
+      {clientRender && (
+        <div className="h-78 w-full max-w-hdPlus md:col-span-full bg-gray-400 animate-pulse" />
+      ) || (
+        <img
+          className="w-full max-w-hdPlus md:col-span-full px-5"
+          src="https://links.papareact.com/dyz"
+          alt="banner"
+        />
+      )}
 
-      {products.length > 0 && (
+      {!clientRender && (
         <div className="md:col-span-2 h-full p-5 xl:p-0 -m-5 xl:m-0">
           {products.slice(4,5).map(({id, title, price, description, category, image}) => (
             <Product
@@ -42,7 +52,7 @@ const ProductFeed = ({products = []}) => {
         </div>
       )}
 
-      {products.length > 0 && products.slice(5, products.length).map(({id, title, price, description, category, image}) => (
+      {!clientRender && products.slice(5, products.length).map(({id, title, price, description, category, image}) => (
         <Product
           key={id}
           id={id}
--- src/components/ProductLoader.jsx
+++ src/components/ProductLoader.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import {StarIcon} from '@heroicons/react/solid'
 
 const ProductLoader = () => {
   return (
-    <div>
-      
+    <div className="relative flex flex-col m-5 p-10 bg-white z-10">
+      <div className="absolute top-2 right-2 h-4 w-1/4 bg-gray-300 animate-pulse rounded-md"/>
+
+      <div className="w-4/5 min-h max-w-50 object-contain aspect-square mx-auto bg-gray-400 animate-pulse rounded-md" />
+
+      <div className="h-5.5 mt-3 mb-0.5 animate-pulse bg-gray-300 rounded-md"/>
+      <div className="h-5.5 w-1/4 mb-3 animate-pulse bg-gray-300 rounded-md"/>
+
+      <div className="flex">
+        <StarIcon className="h-5 text-gray-400 animate-pulse rounded-md"/>
+        <StarIcon className="h-5 text-gray-400 animate-pulse rounded-md"/>
+      </div>
+
+      <div className="h-4 mt-2 mb-0.5 bg-gray-300 animate-pulse rounded-md"/>
+      <div className="h-4 w-2/3 mb-2 bg-gray-300 animate-pulse rounded-md"/>
+
+      <div className="h-6 w-1/5 mb-5 bg-gray-300 animate-pulse rounded-md"/>
+
+
+      <div className="flex items-center space-x-2 -mt-5 py-5">
+        <div className="h-5 w-1/5 bg-gray-300 animate-pulse rounded-md" />
+
+        <div className="h-4 w-2/5 bg-gray-300 animate-pulse rounded-md"/>
+      </div>
+
+      <button className="h-9 p-2 button animate-pulse" onClick={() => console.log('logging dulu')}>
+        <span className="h-5 w-1/3 text-center bg-yellow-500 animate-pulse" />
+      </button>
     </div>
   )
 }
